b3db0d14d994ec8e994d87bb56951cb1aef40bcd
Ajay Srinivasan <ajay@datasisar.com>
2016-03-31 21:15:02 +0530

    -> added version 7

---
+++ scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vibrantWebsiteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the vibrantWebsiteApp
+ */
+angular.module('vibrantWebsiteApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ scripts/controllers/contact.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vibrantWebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vibrantWebsiteApp
+ */
+angular.module('vibrantWebsiteApp')
+  .controller('contactCtrl', function ($scope) {
+    $scope.temp ="sdf"
+  });
